f550a9fbba451075b1dc2bf24252ca30a153dda5
AyushKarnawat <ayush.karnawat97@gmail.com>
2017-02-19 18:42:13 -0500

    Updated state.scm

--- state.scm
+++ state.scm
@@ -1,22 +1,24 @@
-;This file is where the state is stored
-;Any function in this file has to do with reading or writing from the state
-;In order to get the state, functions just need to ask for S
-;The state is just a list of (name value) pairs for variables
+; This file is where the state is stored
+; Any function in this file has to do with reading or writing from the state
+; In order to get the state, functions just need to ask for S
+
+; The state is just a list of (name value) pairs for variables.
+; i.e. The state S is represented by ((name value) (name value) ... (name value)).
 (define S '())
 
-;In order to reset the state, functions will need to use setState. This should only be used by functions in this file.
+; In order to reset the state, functions will need to use setState. This should only be used by functions in this file.
 (define setState
   (lambda (x)
     (set! S x)))
 
-;This function adds a single variable (a (name value) pair) to the state
-;If that variable is already in the state, it removes the old pair and adds the new one
+; This function adds a single variable (a (name value) pair) to the state
+; If that variable is already in the state, it removes the old pair and adds the new one
 (define addVar
   (lambda (n v)
     (setState (assembleState (removeVar n S) ; reassemble the state using the new state after removing the variable
                              (cons (cons n (cons v '())) '()) )))) ; as well as the new variable pair
 
-;This function is a recursive function that looks through the current state s 
+; This function is a recursive function that looks through the current state s 
 ; and removes the first instance of a variable with the name n.
 (define removeVar
   (lambda (n s)
