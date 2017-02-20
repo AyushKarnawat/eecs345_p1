b82cbf3d07115933fe6b684161500417b16110e5
Steven Leonis <Spwizzard2@gmail.com>
2017-02-20 14:15:03 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	interpret.scm

--- interpret.scm
+++ interpret.scm
@@ -4,7 +4,41 @@
 (load "simpleParser.scm")
 (load "state.scm")
 
-(define s 'a)
+(define S '())
+
+;In order to reset the state, functions will need to use setState. This should only be used by functions in this file.
+(define setState
+  (lambda (x)
+    (set! S x)))
+
+(define addVar
+  (lambda (n v)
+    (setState (assembleState (removeVar n S) ; reassemble the state using the new state after removing the variable
+                             (cons (cons n (cons v '())) '()) )))) ; as well as the new variable pair
+
+;This function is a recursive function that looks through the current state s 
+; and removes the first instance of a variable with the name n.
+(define removeVar
+  (lambda (n s)
+    (cond
+      ((null? s) '())
+      ((eq? n (car (car s))) (cdr s))
+      (else (cons (car s) (removeVar n (cdr s)))) )))
+
+;This is a helper function used by addVar to reassemble the state with the new variable pair
+(define assembleState
+  (lambda (s v) ;The new state and the new variable pair
+    (cond
+      ((null? s) v) ;If s is empty, return the new variable pair to append to the end
+      (else (cons (car s) (assembleState (cdr s) v))) )))
+    
+(define declareVar
+  (lambda (l)
+    (cond
+      ((null? (cdr l)) (addVar (car l) '())) ;
+      ; consider cases of assignment coming after var
+      (else (addVar (car l) (car (cdr l)))) )))
+
 
 (define interpret
   (lambda (l)
@@ -26,8 +60,3 @@
       ((eq? (car l) 'while) loop(cdr l));     call while on rest of statement
       (else
        (error 'Not a valid statement!)) )))
-
-;Function that takes a variable declaration statement and executes it
-(define declareVar
-  (lambda (l)
-    ()))
--- state.scm
+++ state.scm
@@ -1,9 +1,51 @@
-;This file is where the state is stored
-;Any function in this file has to do with reading or writing from the state
-;In order to get the state, functions just need to ask for S
+; This file is where the state is stored.
+; All the functions in this file have to do with reading or writing from the state.
+; In order to get the state, functions just need to ask for S.
+
+
+; The state S is a list of (name value) pairs for variables.
 (define S '())
 
-;In order to reset the state, functions will need to use setState. This should only be used by functions in this file.
+; In order to reset the state, functions will need to use setState. 
+; This should only be used by functions in this file.
+; 
+; params:
+;   x: the new value (in this case a list) to set the state S to
 (define setState
   (lambda (x)
     (set! S x)))
+
+; Adds a single variable (a (name value) pair) to the state.
+; If that variable is already in the state, it removes the old pair and adds the new one.
+; 
+; params:
+;   n: name of variable
+;   v: value assigned to that variable
+(define addVar
+  (lambda (n v)
+    (setState (assembleState (removeVar n S) ; reassemble the state using the new state after removing the variable
+                             (cons (cons n (cons v '())) '()) )))) ; as well as the new variable pair
+
+; This function is a recursive function that looks through the current state s 
+; and removes the first instance of a variable with the name n.
+; 
+; params:
+;   n: variable with name n (i.e. 'a)
+;   s: current state
+(define removeVar
+  (lambda (n s)
+    (cond
+      ((null? s) '())
+      ((eq? n (car (car s))) (cdr s))
+      (else (cons (car s) (removeVar n (cdr s)))) )))
+
+; Helper function used by addVar to reassemble the state with the new (name value) pair.
+; 
+; params:
+;   s: new state
+;   v: new (name value) pair
+(define assembleState
+  (lambda (s v)
+    (cond
+      ((null? s) v) ; If s is empty, return the new variable pair to append to the end
+      (else (cons (car s) (assembleState (cdr s) v))) )))
