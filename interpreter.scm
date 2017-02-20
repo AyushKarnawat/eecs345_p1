1b127921afc01f177550543e31410b2c1400e158
ayushkarnawat <ayush.karnawat97@gmail.com>
2017-02-20 04:54:27 -0500

    Style changes

--- interpreter.scm
+++ interpreter.scm
@@ -1,30 +1,39 @@
 ; The main function, it gets the parse tree and divides up the statements
-; which are then read in sequential order
-; 
-; Functions in this file can only access the state through other functions in states.scm
+; which are then read in sequential order.
+; Functions in this file can only access the state through other functions in states.scm.
+
 
 (load "simpleParser.scm")
 (load "state.scm")
 
+; Parses the given filename into a tree (through the parser function provided), 
+; and pass the parse tree onto the runStatementList so that it can be evaluated.
+; 
+; params:
+;   file: the name of the file to interpret and execute. 
 (define interpret
-  (lambda (l)
-    ; Parse the given filename into a tree
+  (lambda (file)
     (cond
-      ((null? (parser l)) '()) ;assuming l is the filename
-      (else (runStatementList (parser l)) ))))
+      ((null? (parser file)) '())
+      (else (runStatementList (parser file))) )))
 
-; This function runs the entire statement tree recursively by using runStatement multiple times
-; l should be the parsed tree in list format i.e ((var x) (= x 10)...)
+; Runs the entire statement tree recursively by using runStatement multiple times.
+; 
+; params:
+;   l: the parsed tree in list format i.e ((var x) (= x 10) ... (return x))
 (define runStatementList
   (lambda (l)
     (cond 
       ((null? l) '())
-      ((null? (cdr l)) (runStatement (car l))) ; if there is the only statement in the list to be evaluated, do so
-      (else (cons (runStatement (car l))       ; call runStatement on the first list  
-                  (runStatementList (cdr l)) )))))  ;and on the rest of the list
+      ((null? (cdr l)) (runStatement (car l)))      ; if there is the only one statement in the list to be evaluated
+      (else (cons (runStatement (car l))            ; call runStatement on the first list  
+                  (runStatementList (cdr l)) )))))  ; and on the rest of the list
       
-; Runs the line it is given
-; If run statements finds a return, it returns the proper value
+; Runs the given statement by calling the necessary functions.
+; When the function finds a return statement, it returns the value of the given variable name.
+; 
+; params:
+;   l: statements (i.e. (var x), (= x 10), (return x) ...)
 (define runStatement
   (lambda (l)
     (cond
@@ -37,11 +46,14 @@
       (else
        (error "Not a valid statement!")) )))
 
-; Function that takes a variable declaration statement and executes it
+; Function that takes a variable declaration statement and executes it.
+; 
+; params:
+;   l: variable declaration statement (i.e. )
 (define declareVar
   (lambda (l)
     (cond
-      ((null? (cdr l)) (addVar (car l) '())) ; if the only given statement is the variable name, assign its value to an empty list
+      ((null? (cdr l)) (addVar (car l) '()))    ; if the only given statement is the variable name, assign its value to an empty list
       (else (addVar (car l) (car (car l)))) ))) ; if an variable name and its value are given
       
 (define assign
@@ -52,14 +64,14 @@
 (define return
   (lambda (l)
     (cond
-      () )))
+      (null? ()) )))
     
 (define conditional
   (lambda (l)
     (cond
-      () )))
+      (null? ()) )))
 
 (define loop
   (lambda (l)
     (cond
-      () )))
+      (null? ()) )))
--- state.scm
+++ state.scm
@@ -1,18 +1,26 @@
-; This file is where the state is stored
-; Any function in this file has to do with reading or writing from the state
-; In order to get the state, functions just need to ask for S
+; This file is where the state is stored.
+; All the functions in this file have to do with reading or writing from the state.
+; In order to get the state, functions just need to ask for S.
 
-; The state is just a list of (name value) pairs for variables.
-; i.e. The state S is represented by ((name value) (name value) ... (name value)).
+
+; The state S is a list of (name value) pairs for variables.
 (define S '())
 
-; In order to reset the state, functions will need to use setState. This should only be used by functions in this file.
+; In order to reset the state, functions will need to use setState. 
+; This should only be used by functions in this file.
+; 
+; params:
+;   x: the new value (in this case a list) to set the state S to
 (define setState
   (lambda (x)
     (set! S x)))
 
-; This function adds a single variable (a (name value) pair) to the state
-; If that variable is already in the state, it removes the old pair and adds the new one
+; Adds a single variable (a (name value) pair) to the state.
+; If that variable is already in the state, it removes the old pair and adds the new one.
+; 
+; params:
+;   n: name of variable
+;   v: value assigned to that variable
 (define addVar
   (lambda (n v)
     (setState (assembleState (removeVar n S) ; reassemble the state using the new state after removing the variable
@@ -20,6 +28,10 @@
 
 ; This function is a recursive function that looks through the current state s 
 ; and removes the first instance of a variable with the name n.
+; 
+; params:
+;   n: variable with name n (i.e. 'a)
+;   s: current state
 (define removeVar
   (lambda (n s)
     (cond
@@ -27,9 +39,13 @@
       ((eq? n (car (car s))) (cdr s))
       (else (cons (car s) (removeVar n (cdr s)))) )))
 
-; This is a helper function used by addVar to reassemble the state with the new (name value) pair
+; Helper function used by addVar to reassemble the state with the new (name value) pair.
+; 
+; params:
+;   s: new state
+;   v: new (name value) pair
 (define assembleState
-  (lambda (s v) ; The new state and the new variable pair
+  (lambda (s v)
     (cond
       ((null? s) v) ; If s is empty, return the new variable pair to append to the end
       (else (cons (car s) (assembleState (cdr s) v))) )))
