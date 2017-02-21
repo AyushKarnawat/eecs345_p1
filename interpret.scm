2e7274ae12da15831a2004433f2124a9bd3d3e68
Steven Leonis <Spwizzard2@gmail.com>
2017-02-20 20:50:22 -0500

    Final version of Programming 1

--- interpret.scm
+++ interpret.scm
@@ -1,62 +1,104 @@
-;The main function, it gets the parse tree and divides up the statements
-; which are then read in sequential order
-;Functions in this file can only access the state through other functions in states.scm
+; Team members: Steven Leonis, Steven Xie, Ayush Karnawat
+
+; The main function, it gets the parse tree and divides up the statements
+; which are then read in sequential order.
+; Functions in this file can only access the state through other functions in states.scm.
+
 (load "simpleParser.scm")
 (load "state.scm")
 
-(define S '())
-
-;In order to reset the state, functions will need to use setState. This should only be used by functions in this file.
-(define setState
-  (lambda (x)
-    (set! S x)))
-
-(define addVar
-  (lambda (n v)
-    (setState (assembleState (removeVar n S) ; reassemble the state using the new state after removing the variable
-                             (cons (cons n (cons v '())) '()) )))) ; as well as the new variable pair
-
-;This function is a recursive function that looks through the current state s 
-; and removes the first instance of a variable with the name n.
-(define removeVar
-  (lambda (n s)
-    (cond
-      ((null? s) '())
-      ((eq? n (car (car s))) (cdr s))
-      (else (cons (car s) (removeVar n (cdr s)))) )))
-
-;This is a helper function used by addVar to reassemble the state with the new variable pair
-(define assembleState
-  (lambda (s v) ;The new state and the new variable pair
-    (cond
-      ((null? s) v) ;If s is empty, return the new variable pair to append to the end
-      (else (cons (car s) (assembleState (cdr s) v))) )))
-    
-(define declareVar
-  (lambda (l)
-    (cond
-      ((null? (cdr l)) (addVar (car l) '())) ;
-      ; consider cases of assignment coming after var
-      (else (addVar (car l) (car (cdr l)))) )))
-
-
+; Parses the given filename into a tree (through the parser function provided), 
+; and pass the parse tree onto the runStatementList so that it can be evaluated.
+; 
+; params:
+;   file: the name of the file to interpret and execute. 
 (define interpret
-  (lambda (l)
+  (lambda (file)
     (cond
-      ((null? l) '())
-      (else
-       (runStatement (car l) )))))
+      ((null? (parser file)) '())
+      (else (runStatementList (parser file))) )))
 
-; Runs the line it is given.
-; if run statements finds a return, it prints(return?) the proper value.
+; Runs the entire statement tree recursively by using runStatement multiple times.
+; 
+; params:
+;   l: the parsed tree in list format i.e ((var x) (= x 10) ... (return x))
+(define runStatementList
+  (lambda (l)
+    (cond 
+      ((null? l) '())
+      ((null? (cdr l)) (runStatement (car l)))  ; if there is the only one statement in the list to be evaluated
+      (else (runStatement (car l))              ; call runStatement on the first list  
+            (runStatementList (cdr l))) )))     ; and on the rest of the list
+      
+; Runs the given statement by calling the necessary functions.
+; When the function finds a return statement, it returns the value of the given variable name.
+; 
+; params:
+;   l: statements (i.e. (var x), (= x 10), (return x) ...)
 (define runStatement
   (lambda (l)
     (cond
       ((null? l) '())
-      ((eq? (car l) 'var) declareVar(cdr l)); call varaible declaration on rest of the statement
-      ((eq? (car l) '=) assign(cdr l));       call assignment on rest of statement
-      ((eq? (car l) 'return) return(cdr l));  call return on rest of statement
-      ((eq? (car l) 'if) conditional(cdr l)); call if on rest of statement
-      ((eq? (car l) 'while) loop(cdr l));     call while on rest of statement
+      ((eq? (car l) 'var) (declareVar (cdr l))) ; call variable declaration on rest of the statement
+      ((eq? (car l) '=) (assign (cdr l)))       ; call assignment on rest of statement
+      ((eq? (car l) 'return) (return (cdr l)))  ; call return on rest of statement
+      ((eq? (car l) 'if) (conditional (cdr l))) ; call if on rest of statement
+      ((eq? (car l) 'while) (loop (cdr l)))     ; call while on rest of statement
       (else
-       (error 'Not a valid statement!)) )))
+       (error "Not a valid statement!")) )))
+
+; Function that takes a variable declaration statement and executes it.
+; 
+; params:
+;   l: variable declaration statement (i.e. '(x) or '(x 5)) for a (name value) pair
+(define declareVar
+  (lambda (l)
+    (cond
+      ((null? (cdr l)) (addVar (car l) '())) ; if the only given statement is the variable name, assign its value to an empty list
+      (else (addVar (car l) (evalExp (cadr l)))) ))) ; if an variable name and its value are given
+
+; Assigns a value to a variable.
+; 
+; params:
+;   l: a (name value) pair related to the variable
+(define assign
+ (lambda (l)
+  (cond
+    ((null? l) '())
+    ((declared? (car l)) (addVar (car l) (evalExp (cadr l))))
+    (else ((declared? (car l)) (error "Variable is not declared."))) ))) ; if the variable is avaliable in the state, assign the value to the var
+
+; Returns a given expression or variable.
+;
+; examples: (return (* x x)) 
+;           (return x) 
+;           (return (* x (+ x x))) 
+; params:
+;   l: the list passed in
+(define return
+  (lambda (l)
+    (cond
+      ((null? l) (error "Return was passed a null list"))
+      ((eq? (evalExp (car l)) #t) 'true)
+      ((eq? (evalExp (car l)) #f) 'false)
+      (else (evalExp (car l))) )))
+
+; example of if statement as passed:
+;      expr    then       else (in this case, if)
+; (if (> x y) (return x) (if (> (* x x) y) ... )
+; Must evaluate expression after itself, then execute stuff after    
+(define conditional
+  (lambda (l)
+    (cond
+      ((evalExp (car l)) (runStatement (car (cdr l)))) ; test whether (car l) is true
+      ((null? (cddr l)) '())
+      (else (runStatement(caddr l))) ))) ;if expr to evaluate is false
+
+; As the name suggests, loop until test-expr is true
+;   test-expr using evalExpr | body of loop
+; (while (!= (% y x) 3) (= y (+ y 1))) 
+(define loop
+  (lambda (l)
+    (cond
+      ((not (evalExp (car l))) '())
+      (else (runStatement (cadr l)) (loop l)) ))) ; check statement, and loop again
--- state.scm
+++ state.scm
@@ -1,6 +1,8 @@
+; Team members: Steven Leonis, Steven Xie, Ayush Karnawat
+
 ; This file is where the state is stored.
 ; All the functions in this file have to do with reading or writing from the state.
-; In order to get the state, functions in this file just need to ask for S.
+; In order to get the state, functions just need to ask for S.
 
 ; The state S is a list of (name value) pairs for variables.
 (define S '())
@@ -13,6 +15,26 @@
 (define setState
   (lambda (x)
     (set! S x)))
+
+; Function declared looks through state S and returns true if variable is declared.
+;
+; params:
+;   n: name of variable
+(define declared?
+  (lambda (n)
+    (recursiveDeclared? n S) ))
+
+; Helper function that allows outside functions to know if a variable is declared.
+;
+; params:
+;   n: name of variable 
+;   s: current state
+(define recursiveDeclared?
+  (lambda (n s)
+    (cond
+      ((null? s) #f)              ; base case: var not in state
+      ((eq? n (car (car s))) #t)  ; if var found, return true
+      (else (recursiveDeclared? n (cdr s)) ) )))
 
 ; Adds a single variable (a (name value) pair) to the state.
 ; If that variable is already in the state, it removes the old pair and adds the new one.
@@ -51,8 +73,44 @@
 
 ; This function evaluates an expression recursively, dealing with both numerical and boolean operators
 ; In this way, it is performing both M_value and M_boolean operations
-
-(define evaluateExpression
+(define evalExp
   (lambda (l)
     (cond
-      
+      ((null? l) '())
+      ((number? l) l)
+      ((eq? l 'true) #t)
+      ((eq? l 'false) #f)
+      ((atom? l) (if (declared? l) (if (not (null? (valueOf l S))) (valueOf l S)
+                                                                   (error "Variable is not assigned!"))
+                                   (error "Variable was not declared")))
+      ((eq? (car l) '+) (+ (evalExp (cadr l)) (evalExp (caddr l))))
+      ((eq? (car l) '-) (if (null? (cddr l)) (* (evalExp (cadr l)) -1)
+                                             (- (evalExp (cadr l)) (evalExp (caddr l))) ))
+      ((eq? (car l) '*) (* (evalExp (cadr l)) (evalExp (caddr l))))
+      ((eq? (car l) '/) (quotient (evalExp (cadr l)) (evalExp (caddr l))))
+      ((eq? (car l) '%) (modulo (evalExp (cadr l)) (evalExp (caddr l))))
+      ((eq? (car l) '==) (eq? (evalExp (cadr l)) (evalExp (caddr l))))
+      ((eq? (car l) '!=) (not (eq? (evalExp (cadr l)) (evalExp (caddr l)))))
+      ((eq? (car l) '<) (< (evalExp (cadr l)) (evalExp (caddr l))))
+      ((eq? (car l) '>) (> (evalExp (cadr l)) (evalExp (caddr l))))
+      ((eq? (car l) '<=) (<= (evalExp (cadr l)) (evalExp (caddr l))))
+      ((eq? (car l) '>=) (>= (evalExp (cadr l)) (evalExp (caddr l))))
+      ((eq? (car l) '&&) (and (evalExp (cadr l)) (evalExp (caddr l))))
+      ((eq? (car l) '||) (or (evalExp (cadr l)) (evalExp (caddr l))))
+      ((eq? (car l) '!) (not (evalExp (cadr l))))
+      (else (if (declared? (car l)) (valueOf (car l) S) (error ("Variable was not declared!")))))))
+
+; A recursive function that finds the value (int or bool) of a declared variable in the state
+
+(define valueOf
+  (lambda (x l)
+    (cond
+      ((null? l) '())
+      ((eq? (caar l) x) (cadar l))
+      (else (valueOf x (cdr l))) )))
+
+; Used under MIT License The Little Schemer. All Rights Reserved
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+; End Copyright
